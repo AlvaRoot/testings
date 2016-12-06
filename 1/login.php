fa19cbcbb8c9bd7dd8e5c757a56ff6cefe04809c
AlvaRoot <alvaropoblete.monsalve@gmail.com>
2016-12-06 07:47:17 -0300

    Add files via upload

--- 1/login.php
+++ 1/login.php
@@ -1,6 +1,32 @@
 <?php
 //defined('BASEPATH') OR exit('No direct script access allowed');
 
+require_once("conexion.php");
+
+if ($result->num_rows > 0) {     
+ }
+ $row = $result->fetch_array(MYSQLI_ASSOC);
+ if (password_verify($password, $row['password'])) { 
+ 
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username;
+    $_SESSION['start'] = time();
+    $_SESSION['expire'] = $_SESSION['start'] + (5 * 60);
+
+    echo "Bienvenido! " . $_SESSION['username'];
+    echo "<br><br><a href=panel-control.php>Panel de Control</a>"; 
+
+ } else { 
+   echo "Username o Password estan incorrectos.";
+
+   echo "<br><a href='login.html'>Volver a Intentarlo</a>";
+ }
+
+
+
+
+
+
 ?>
 <!DOCTYPE html>
 
